ad845a59dcb89039f10905a1284c04110837672a
Lucas Tiago <eulukasthyago@outlook.com>
2021-04-02 13:30:45 -0300

    melhorias

--- pages/post/hellow-there.tsx
+++ pages/post/hellow-there.tsx
@@ -1,3 +1,4 @@
+import Navigation from '@components/Navigation';
 import Head from 'next/head';
 import Image from 'next/image';
 
@@ -8,6 +9,7 @@ export default function Hello () {
                 <title>Hello there! - My Blog</title>
                 <meta property="og:title" content="Hello there! - My Blog" />
             </Head>
+            <Navigation />
             <article>
                 <h1>Hello There!</h1>
                 <Image
@@ -15,7 +17,7 @@ export default function Hello () {
                     width={640}
                     height={427}
                 />
-                <p>Foto de <a href='https://www.pexels.com/pt-br/@neo-853421?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels'>Neo</a> no <a href='https://www.pexels.com/pt-br/foto/linhas-de-codigo-2653362/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels'>Pexels</a></p>
+                <p>Foto de <a href='https://www.pexels.com/pt-br/@neo-853421?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels' target="_blank">Neo</a> no <a href='https://www.pexels.com/pt-br/foto/linhas-de-codigo-2653362/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels' target="_blank">Pexels</a></p>
             </article>
         </>
     );
