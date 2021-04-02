7de07b2fdd45ce4f688b2e355f3ef4752424497c
Lucas Tiago <eulukasthyago@outlook.com>
2021-04-02 13:25:50 -0300

    Finalização do curso

---
+++ pages/post/hellow-there.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Hello () {
+    return(
+        <>
+            <Head>
+                <title>Hello there! - My Blog</title>
+                <meta property="og:title" content="Hello there! - My Blog" />
+            </Head>
+            <article>
+                <h1>Hello There!</h1>
+                <Image
+                    src='/post/post01.jpg'
+                    width={640}
+                    height={427}
+                />
+                <p>Foto de <a href='https://www.pexels.com/pt-br/@neo-853421?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels'>Neo</a> no <a href='https://www.pexels.com/pt-br/foto/linhas-de-codigo-2653362/?utm_content=attributionCopyText&utm_medium=referral&utm_source=pexels'>Pexels</a></p>
+            </article>
+        </>
+    );
+}
